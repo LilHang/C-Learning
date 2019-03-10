38c29225c4723cb433aaf5cecb3efde885514e09
LiH <lihang0614@163.com>
2019-03-10 15:10:59 +0800

    优化穷举

--- code/4-12.c
+++ code/4-12.c
@@ -4,14 +4,16 @@ int main(void) {
     int men, women, children;
     int count = 0;
 
-    for (men = 0; men <= 45; men++)
-        for (women = 0; women <= 45; women++)
-            for (children = 0; children <= 45; children++)
-                if (men + women + children == 45 &&
-                    men * 3 + women * 2 + children * 0.5 == 45)
-                    printf("men = %d, women = %d, children = %d\n", men, women,
-                           children);
-    count++;
-    printf("total: %d ", count);
+    for (men = 0; men <= 15; men++)
+        for (women = 0; women <= 22; women++) {
+            children = 45 - men - women;
+            if (men + women + children == 45 &&
+                men * 3 + women * 2 + children * 0.5 == 45) {
+                printf("men = %d, women = %d, children = %d\n", men, women,
+                       children);
+                count++;
+            }
+        }
+    printf("total: %d\n", count);
     return 0;
 }
