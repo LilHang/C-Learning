9e53ff48fd39f6f3ffb3d4c5790306aaf29b31c7
LiH <lihang0614@163.com>
2019-03-10 20:55:25 +0800

    修改为全局变量

--- code/5-9.c
+++ code/5-9.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-
+double f = 1;
 double fact_s(int n);
 int main(void) {
     int i, n;
     printf("input n: ");
     scanf("%d", &n);
     for (i = 1; i <= n; i++) {
-        printf("%d! =%.0f\n", i, fact_s(i));
+        printf("%3d! =%.0f\n", i, fact_s(i));
     }
     return 0;
 }
 
 double fact_s(int n) {
-    static double f = 1;
     f = f * n;
     return f;
 }
