6928413b14f181e8b780ffe1865f07072f38cabc
LiH <lihang0614@163.com>
2019-03-13 20:38:57 +0800

    二维数组

---
+++ code/7-7.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(void) {
+    int a[3][2];
+    int i, j, row, col;
+    printf("enter 6 integers: \n");
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 2; j++)
+            scanf("%d", &a[i][j]);
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 2; j++)
+            printf("%4d", a[i][j]);
+        printf("\n");
+    }
+    row = col = 0;
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 2; j++)
+            if (a[i][j] > a[row][col]) {
+                row = i;
+                col = j;
+            }
+    printf("max = a[%d][%d] = %d\n", row, col, a[row][col]);
+
+    return 0;
+}
