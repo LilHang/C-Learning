a2807d1af2645b4bca0e35d52465d4c34e7bab6e
LiH <lihang0614@163.com>
2019-03-16 20:05:08 +0800

    在线处理

--- code/01-最大子列和.c
+++ code/01-最大子列和.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int divide_and_conquer(int list[], int left, int right);
+int MaxSubseqsum(int list[], int n);
 
 int main(void) {
     int k, i;
@@ -9,49 +9,19 @@ int main(void) {
         scanf("%d", &a[i]);
     }
 
-    printf("%d", divide_and_conquer(a, 0, k - 1));
+    printf("%d", MaxSubseqsum(a, k));
 
     return 0;
 }
-int max3(int a, int b, int c) {
-    int max;
-    if (a > b) {
-        if (a > c) {
-            max = a;
-        } else
-            max = c;
-    } else if (b > c) {
-        max = b;
-    } else
-        max = c;
-    return max;
-}
-int divide_and_conquer(int list[], int left, int right) {
-    int center, i;
-    int maxleftbordersum, leftbordersum, maxrightbordersum, rightbordersum;
-    int maxleftsum, maxrightsum;
-    if (left == right) {
-        if (list[left] > 0)
-            return list[left];
-        else
-            return 0;
+int MaxSubseqsum(int list[], int n) {
+    int i;
+    int thissum = 0, maxsum = 0;
+    for (i = 0; i < n; i++) {
+        thissum += list[i];
+        if (thissum > maxsum)
+            maxsum = thissum;
+        else if (thissum < 0)
+            thissum = 0;
     }
-    center = (left + right) / 2;
-    maxleftsum = divide_and_conquer(list, left, center);
-    maxrightsum = divide_and_conquer(list, center + 1, right);
-
-    maxleftbordersum = 0, leftbordersum = 0;
-    for (i = center; i >= left; i--) {
-        leftbordersum += list[i];
-        if (leftbordersum > maxleftbordersum)
-            maxleftbordersum = leftbordersum;
-    }
-    maxrightbordersum = 0, rightbordersum = 0;
-    for (i = center + 1; i <= right; i++) {
-        rightbordersum += list[i];
-        if (rightbordersum > maxrightbordersum)
-            maxrightbordersum = rightbordersum;
-    }
-
-    return max3(maxleftsum, maxrightsum, maxleftbordersum + maxrightbordersum);
+    return maxsum;
 }
