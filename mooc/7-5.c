b52d55584502c5a353a0d1a46616ac8f43f18136
LiH <lihang0614@163.com>
2019-04-02 22:20:34 +0800

    倒三角 循环法

--- mooc/7-5.c
+++ mooc/7-5.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 int main(void) {
-    printf("* * * *\n");
-    printf(" * * *\n");
-    printf("  * *\n");
-    printf("   *\n");
-
+    int i, j, k;
+    for (i = 1; i <= 4; i++) {      //控制层数
+        for (j = i - 1; j > 0; j--) //控制空格数
+            printf(" ");
+        for (k = 4 - i + 1; k > 0; k--) //控制*数，注意最后的空格
+            if (k != 1)
+                printf("* ");
+            else
+                printf("*\n");
+    }
     return 0;
 }
